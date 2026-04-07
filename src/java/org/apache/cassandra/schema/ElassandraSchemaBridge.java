3151dce75f66735f968279a0e8bc90eea51a94b3
maxts <3308569+maxts@users.noreply.github.com>
2026-04-07 22:14:55 +0300

    Add Apache license header to ElassandraSchemaBridge for RAT

--- src/java/org/apache/cassandra/schema/ElassandraSchemaBridge.java
+++ src/java/org/apache/cassandra/schema/ElassandraSchemaBridge.java
@@ -1,12 +1,27 @@
 /*
- * Elassandra: bridge package-private SchemaKeyspace mutation builders for org.elassandra.cluster.SchemaManager.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.cassandra.schema;
 
 import org.apache.cassandra.db.Mutation;
 
 /**
- * Delegates to {@link SchemaKeyspace} static helpers that are otherwise package-private to {@code org.apache.cassandra.schema}.
+ * Elassandra: exposes {@link SchemaKeyspace} mutation helpers to {@code org.elassandra.cluster.SchemaManager}.
+ * Delegates to package-private static methods on {@link SchemaKeyspace}.
  */
 public final class ElassandraSchemaBridge
 {
